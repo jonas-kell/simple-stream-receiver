7d66116adea678c53df8fdcdda9ae79c7ff62240
Jonas Kell <jonas-kell@web.de>
2025-06-14 21:46:30 +0200

    fix: do not print error

--- run.sh
+++ run.sh
@@ -8,7 +8,7 @@ if [[ ! -f "$COMPOSE_DIR/docker-compose.yml" ]]; then
   exit 1
 fi
 
-echo "Starting `docker compose` in $COMPOSE_DIR"
+echo "Starting docker compose in $COMPOSE_DIR"
 cd "$COMPOSE_DIR" || exit 1
 (docker compose up) &
 
